d5cb9433e605ea0c89315a992838cf2fd2c25f89
Binh Le <lebinh190998@gmail.com>
2024-09-17 21:47:38 +0700

    LiveOak0: VarDecl

--- lo2-to-sam-tester-public/src/main/java/assignment2/LiveOak2Compiler.java
+++ lo2-to-sam-tester-public/src/main/java/assignment2/LiveOak2Compiler.java
@@ -1,21 +1,19 @@
 package assignment2;
 
-import edu.utexas.cs.sam.io.SamTokenizer;
-import edu.utexas.cs.sam.io.Tokenizer;
-import edu.utexas.cs.sam.io.Tokenizer.TokenType;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.util.Map;
-import java.util.HashMap;
-
-import java.io.IOException;
 import assignment2.errors.CompilerException;
 import assignment2.errors.SyntaxErrorException;
 import assignment2.errors.TypeErrorException;
-
+import edu.utexas.cs.sam.io.SamTokenizer;
+import edu.utexas.cs.sam.io.Tokenizer;
+import edu.utexas.cs.sam.io.Tokenizer.TokenType;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LiveOak2Compiler {
+
     public static void main(String[] args) throws IOException {
         if (args.length < 2) {
             System.err.println("Usage: java ProgramName inputFile outputFile");
@@ -28,7 +26,11 @@ public class LiveOak2Compiler {
         try {
             String samCode = compiler(inFileName);
             System.out.println(samCode);
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(outFileName))) {
+            try (
+                BufferedWriter writer = new BufferedWriter(
+                    new FileWriter(outFileName)
+                )
+            ) {
                 writer.write(samCode);
             }
         } catch (IOException e) {
@@ -38,12 +40,19 @@ public class LiveOak2Compiler {
     }
 
     // maps identifier -> variable
-    public static Map<String, Variable> variables = new HashMap<String, Variable>();
+    public static Map<String, Variable> variables = new HashMap<
+        String,
+        Variable
+    >();
 
     static String compiler(String fileName) {
         //returns SaM code for program in file
         try {
-            SamTokenizer f = new SamTokenizer(fileName, SamTokenizer.TokenizerOptions.PROCESS_STRINGS);
+            System.out.println("COMPILING...");
+            SamTokenizer f = new SamTokenizer(
+                fileName,
+                SamTokenizer.TokenizerOptions.PROCESS_STRINGS
+            );
             String pgm = getProgram(f);
             return pgm;
         } catch (CompilerException e) {
@@ -56,21 +65,18 @@ public class LiveOak2Compiler {
     }
 
     static String getProgram(SamTokenizer f) throws CompilerException {
-        try {
-            String pgm="";
-            // LiveOak-2
-            // while(f.peekAtKind()!=TokenType.EOF) {
-            //     pgm+= getMethod(f);
-            // }
-            // LiveOak-0
+        String pgm = "";
+        // LiveOak-2
+        // while(f.peekAtKind()!=TokenType.EOF) {
+        //     pgm+= getMethod(f);
+        // }
+
+
+        // LiveOak-0
+        while(f.peekAtKind()!=TokenType.EOF) {
             pgm += getBody(f);
-            return pgm;
-        } catch(Exception e) {
-            String errorMessage = "Fatal error: could not compile program";
-            System.out.println(errorMessage);
-            System.out.println(e);
-            return "STOP\n";
         }
+        return pgm;
     }
 
     static String getMethod(SamTokenizer f) {
@@ -78,9 +84,9 @@ public class LiveOak2Compiler {
         //TODO: add appropriate exception handlers to generate useful error msgs.
         f.check("int"); //must match at begining
         String methodName = f.getString();
-        f.check ("("); // must be an opening parenthesis
+        f.check("("); // must be an opening parenthesis
         String formals = getFormals(f);
-        f.check(")");  // must be an closing parenthesis
+        f.check(")"); // must be an closing parenthesis
         //You would need to read in formals if any
         //And then have calls to getDeclarations and getStatements.
         return null;
@@ -94,16 +100,13 @@ public class LiveOak2Compiler {
         return null;
     }
 
-
     /** LiveOak 0
-    **/
+     **/
     static String getBody(SamTokenizer f) throws CompilerException {
         String body = "";
 
-        // while not Block, declare Var
-        String typeString = f.getWord();
-        Type varType = Type.fromString(typeString);
-        while (varType != null) {
+        // while starting as a word, declare Var
+        while (f.peekAtKind() == TokenType.WORD) {
             // VarDecl will store variable in Hashmap: identifier -> { type: TokenType, relative_address: int }
             parseVarDecl(f);
         }
@@ -118,25 +121,33 @@ public class LiveOak2Compiler {
         // handle Type
         String typeString = f.getWord();
         Type varType = Type.fromString(typeString);
-        if (varType != null) {
-            throw new TypeErrorException("Invalid type: " + typeString, f.lineNo());
+        if (varType == null) {
+            throw new TypeErrorException(
+                "Invalid type: " + typeString,
+                f.lineNo()
+            );
         }
-        f.skipToken();
 
         // put variables in hashmap
-        while (f.getCharacter() != ';') {
-            String varName = f.getString();
+        while (f.peekAtKind() == TokenType.WORD) {
+            String varName = f.getWord();
 
             int address = CompilerUtils.getNextAddress(variables);
             Variable variable = new Variable(varName, varType, address);
             variables.put(varName, variable);
 
-            // BUG HERE
-            if(!f.check(',') && f.getCharacter() != ';') {
-                throw new SyntaxErrorException("Expected ',' or `;` after each variable declaration", f.lineNo());
+            if(f.check(',')) {
+                continue;
+            }
+            else if(f.check(';')) {
+                break;
+            } else {
+                throw new SyntaxErrorException(
+                    "Expected ',' or `;` after each variable declaration",
+                    f.lineNo()
+                );
             }
         }
-        f.skipToken(); // skip ';'
     }
 
     static String getIdentifier(SamTokenizer f) {
