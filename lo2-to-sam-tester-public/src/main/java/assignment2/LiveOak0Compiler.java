3b68c15f156f3077b2c5e3a6b68157b409198ec4
Binh Le <lebinh190998@gmail.com>
2024-10-17 19:02:41 +0700

    DONE!

--- lo2-to-sam-tester-public/src/main/java/assignment2/LiveOak0Compiler.java
+++ lo2-to-sam-tester-public/src/main/java/assignment2/LiveOak0Compiler.java
@@ -617,7 +617,7 @@ public class LiveOak0Compiler {
         switch (op) {
             // TODO: string bitwise
             case '~':
-                return "PUSHIMM -1\nTIMES\nPUSHIMM 1\nSUB\n";
+                return "PUSHIMM -1\nTIMES\n";
             case '!':
                 return "PUSHIMM 1\nADD\nPUSHIMM 2\nMOD\n";
             default:
